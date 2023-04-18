23d7b3fb40a8ffe568aa757318500570fec928ef
bryancury3r <gabriel_bryancury@outlook.com>
2023-04-17 22:02:16 -0300

    desafio-02

---
+++ exer02/script.js
@@ -0,0 +1,7 @@
+const base = parseInt(prompt("digite a base:"));
+
+const altura = parseInt(prompt("digite a altura:"));
+
+const retangulo = base * altura;
+
+document.write(`A área do retângulo corresponde a: ${retangulo}`);
