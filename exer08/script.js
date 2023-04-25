b1d1fb8894fb6d57194ab24f00f985c7b8756516
bryancury3r <gabriel_bryancury@outlook.com>
2023-04-25 20:40:38 -0300

    exercicio 09

--- exer08/script.js
+++ exer08/script.js
@@ -1,7 +1,9 @@
 const valor = parseInt(prompt("digite um valor:"));
 
-if (valor < 10) {
-  document.write(`O valor ${valor} é menor que 10`);
-} else {
+if (valor > 10) {
   document.write(`O valor ${valor} é maior que 10`);
+} else if (valor === 10) {
+  document.write(`O valor ${valor} é igual a 10`);
+} else {
+  document.write(`O valor ${valor} é menor que 10`);
 }
---
+++ exer09/script.js
@@ -0,0 +1,7 @@
+const valor = parseInt(prompt("digite um valor:"));
+
+if (valor >= 0) {
+  document.write("Positivo");
+} else {
+  document.write("negativo!");
+}
