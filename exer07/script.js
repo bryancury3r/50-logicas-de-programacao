fa446e9e7c49e9f96cf06d64b026d65da6564521
bryancury3r <gabriel_bryancury@outlook.com>
2023-04-25 20:23:08 -0300

    exercicio 08

---
+++ exer07/script.js
@@ -0,0 +1,9 @@
+const nota1 = parseInt(prompt("digite primeira nota:"));
+
+const nota2 = parseInt(prompt("digite sugunda nota:"));
+
+const nota3 = parseInt(prompt("digite terceira nota:"));
+
+const media = nota1 * 2 + nota2 * 3 + (nota3 * 5) / 10;
+
+document.write(`sua media é: ${media}`);
---
+++ exer08/script.js
@@ -0,0 +1,7 @@
+const valor = parseInt(prompt("digite um valor:"));
+
+if (valor < 10) {
+  document.write(`O valor ${valor} é menor que 10`);
+} else {
+  document.write(`O valor ${valor} é maior que 10`);
+}
