37ae908e6768a7f9ca707fd832bc17652d56990a
bryancury3r <gabriel_bryancury@outlook.com>
2023-06-26 20:22:35 -0300

    exer 32

--- exer32/script.js
+++ exer32/script.js
@@ -1,5 +1,9 @@
-const tabuada = parseInt(prompt("Digite a tabuada:"));
+let x = 0;
 
-for (let i = 1; i <= tabuada; i++) {
-  document.write(`${tabuada} x ${i} = ${tabuada * i}<br>`);
+for (let i = 1; i <= 10; i++) {
+  document.write(`-------------<br>`);
+  for (let j = 1; j <= 10; j++) {
+    x = i * j;
+    document.write(`${i} x ${j} = ${x}<br>`);
+  }
 }
